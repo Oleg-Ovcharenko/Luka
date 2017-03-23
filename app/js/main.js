83b0f7b229239ba6ce6f6103c889fa03a7be48e6
Oleg-Ovcharenko <oleg-enzo@yandex.ua>
2017-03-23 15:44:35 +0200

    add news section

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,10 @@
-window.onload = function() {
-  var menu_btn = document.getElementById('menu_head');
-  var menu_con = document.getElementById('menu_items_container');
-
-  var visible = false;
-
-  menu_btn.onclick = function() {
-    if(visible == true){
-      visible = false;
-      menu_con.style.display = "none";
-    } else {
-      visible = true;
-      menu_con.style.display = "block";
-    }
-  };
-
-  /*=== SLIDER ===*/
-
-  var slides = document.querySelectorAll('#slider .header_slider__slide');
-  var currentSlide = 0;
-  var slideInterval = setInterval(nextSlide,5000);
-
-  function nextSlide() {
-      slides[currentSlide].className = 'header_slider__slide';
-      currentSlide = (currentSlide+1)%slides.length;
-      slides[currentSlide].className = 'header_slider__slide showing';
-  }
-
-};
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: {
+      lat: 49.8337136,
+      lng: 24.0125895
+    },
+    disableDefaultUI: true
+  });
+}
