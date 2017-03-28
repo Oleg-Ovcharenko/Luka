d05804237b4076ca2e927e730ec76269ce63c22b
Oleg-Ovcharenko <oleg-enzo@yandex.ua>
2017-03-28 22:05:23 +0300

    last commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,18 @@
+jQuery(document).ready(function($){
+  console.log(flexslider);
+  $('.slider').flexslider({
+    selector: ".tabs > .tab",
+    animation: "slide"
+  });
+});
+
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: {
-      lat: 49.8337136,
-      lng: 24.0125895
+      lat: 49.8448856,
+      lng: 24.0234585
     },
     disableDefaultUI: true
   });
